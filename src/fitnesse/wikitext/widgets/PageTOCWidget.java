f6f67ebe13daac7ef6983afd9e51653cbf796aa7
Mara Dalponte <huenu@triton.(none)>
2009-03-03 16:20:46 -0200

    Fixed WikiTextTranslatorTest.testTranslation1() error. Some other tes changes

--- src/fitnesse/wikitext/widgets/PageTOCWidget.java
+++ src/fitnesse/wikitext/widgets/PageTOCWidget.java
@@ -1,97 +1,88 @@
 package fitnesse.wikitext.widgets;
-
+ 
 import fitnesse.html.HtmlTag;
 import fitnesse.html.HtmlUtil;
 import fitnesse.wiki.WikiPage;
 import fitnesse.wikitext.WikiWidget;
-
+ 
 /**
- * This widget renders the hierarchical headers of the 
- * page as a TOC (table of contents).
- * @author huenu
- * */
+* This widget renders the hierarchical headers of the
+* page as a TOC (table of contents).
+* @author huenu
+* */
 public class PageTOCWidget extends WikiWidget {
-	public static final String REGEXP = "(?:^!pagecontents([ \t]+-R[0-9]*)?([ \t]+-[fhgp])*?[ \\t]*$)";
-	
-	public PageTOCWidget(ParentWidget parent, String text)
-	{
-		super(parent);	
-	}
-	@Override
-	public String render() throws Exception
-	{
-		WikiPage page = getWikiPage();
-		return buildContentsDiv(page).html();
-	}
-	   
-
-	private HtmlTag buildContentsDiv(WikiPage wikiPage)
-		throws Exception
-	{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-		HtmlTag div =  HtmlUtil.makeDivTag("toc1");
-		HtmlTag contentsDiv = HtmlUtil.makeDivTag("contents");
-		contentsDiv.add(HtmlUtil.makeBold("Index:"));
-		contentsDiv.add(buildListOfHeaders(wikiPage));
-		div.add(contentsDiv);
-		return div;
-	}
-
-	
-	private HtmlTag buildListOfHeaders(WikiPage wikiPage) throws Exception
-	{	
-		String content = wikiPage.getData().getContent();
-		HeaderParser parser= new HeaderParser();
-		return parser.parse(content);
-		
-	}
+  public static final String REGEXP = "(?:^!pagecontents([ \t]+-R[0-9]*)?([ \t]+-[fhgp])*?[ \\t]*$)";
+  
+  public PageTOCWidget(ParentWidget parent, String text)
+  {
+    super(parent);  
+  }
+  @Override
+  public String render() throws Exception
+  {
+    HtmlTag div = HtmlUtil.makeDivTag("toc1");
+    HtmlTag contentsDiv = HtmlUtil.makeDivTag("contents");
+    contentsDiv.add(HtmlUtil.makeBold("Index:"));
+    contentsDiv.add(buildIndexContents(getWikiPage()));
+    div.add(contentsDiv);
+    return div.html();
+  }
+  
+ 
+  private HtmlTag buildIndexContents(WikiPage wikiPage)
+    throws Exception
+  {
+    HeaderParser parser= new HeaderParser();
+    String pageContent=wikiPage.getData().getContent();    
+    return parser.parse(pageContent);
+  }
 }
-
-class HeaderParser {	
-	Boolean done =false;
-	
-	public HtmlTag parse(String content){
-		int level=1;
-		HtmlTag htmlT = processLevel(1,content);		
-		while(htmlT==null){
-			htmlT = processLevel(level++,content);
-		}
-		return htmlT;
-	}	
-	
-	private HtmlTag processLevel(int level, String levelString) {
-		String[] parts = levelString.split("!"+level);
-
-		if(parts.length==1){
-			return null;			
-		}else{
-			HtmlTag list = new HtmlTag("ul");	
-			for (int partnum=1;partnum<parts.length ;partnum++ ) {
-				String part= parts[partnum];				
-				if(part.length()>0){
-					HtmlTag listItem = this.format(part);
-					list.add(listItem);
-					HtmlTag child = this.processLevel(level+1, part);
-					if(child!=null)	list.add(child);
-				}
-			}				
-			return list;
-		}		
-	}
-	/**
-	 * Formats the first line of given string, to make a list item.
-	 * */
-	private HtmlTag format(String header) {
-		HtmlTag listItem = new HtmlTag("li");
-		int idxStop = (header.indexOf('\n')<0)?header.length():header.indexOf('\n');
-		String headerTitle = header.substring(0,idxStop).trim();
-		try {
-			HtmlTag link = HtmlUtil.makeLink("#"+HeaderWidget.anchorName(headerTitle),headerTitle);
-			listItem.add(link);
-		} catch (Exception e) {
-			//The link cannot be build
-			listItem.add(headerTitle);
-		}		
-		return listItem;
-	}
+ 
+class HeaderParser {  
+  Boolean done =false;
+  
+  public HtmlTag parse(String content){
+    int level=1;
+    HtmlTag htmlT = processLevel(1,content);    
+    while(htmlT==null){
+      htmlT = processLevel(level++,content);
+    }
+    return htmlT;
+  }  
+  
+  private HtmlTag processLevel(int level, String levelString) {
+    String[] parts = levelString.split("!"+level);
+ 
+    if(parts.length==1){
+      return null;      
+    }else{
+      HtmlTag list = new HtmlTag("ul");  
+      for (int partnum=1;partnum<parts.length ;partnum++ ) {
+        String part= parts[partnum];        
+        if(part.length()>0){
+          HtmlTag listItem = this.format(part);
+          list.add(listItem);
+          HtmlTag child = this.processLevel(level+1, part);
+          if(child!=null)  list.add(child);
+        }
+      }        
+      return list;
+    }    
+  }
+  /**
+   * Formats the first line of given string, to make a list item.
+   * */
+  private HtmlTag format(String header) {
+    HtmlTag listItem = new HtmlTag("li");
+    int idxStop = (header.indexOf('\n')<0)?header.length():header.indexOf('\n');
+    String headerTitle = header.substring(0,idxStop).trim();
+    try {
+      HtmlTag link = HtmlUtil.makeLink("#"+HeaderWidget.anchorName(headerTitle),headerTitle);
+      listItem.add(link);
+    } catch (Exception e) {
+      //The link cannot be build
+      listItem.add(headerTitle);
+    }    
+    return listItem;
+  }
 }
-
--- src/fitnesse/wikitext/widgets/PageTOCWidgetTest.java
+++ src/fitnesse/wikitext/widgets/PageTOCWidgetTest.java
@@ -5,75 +5,101 @@ import fitnesse.wiki.WikiPageDummy;
 import fitnesse.wiki.WikiPagePath;
 import fitnesse.wikitext.WidgetBuilder;
 
+public class PageTOCWidgetTest extends WidgetTestCase {
 
-public class PageTOCWidgetTest extends WidgetTestCase{
-	
 	@Override
 	protected String getRegexp() {
-	    return PageTOCWidget.REGEXP;
-	 }
-
-    public void testRegexp() throws Exception {
-	    assertMatch("!pagecontents");
+		return PageTOCWidget.REGEXP;
 	}
 
-	public WidgetRoot widgetRoot(String content) throws Exception
-	{
-		return new WidgetRoot(null, 
-				new PagePointer(new WikiPageDummy("RooT",content), 
-						new WikiPagePath()), 
-				WidgetBuilder.htmlWidgetBuilder);
+	public void testRegexp() throws Exception {
+		assertMatch("!pagecontents");
 	}
-	
-	public void testFromMainLevel(){		
+
+	public WidgetRoot widgetRoot(String content) throws Exception {
+		return new WidgetRoot(null, new PagePointer(new WikiPageDummy("RooT",
+				content), new WikiPagePath()), WidgetBuilder.htmlWidgetBuilder);
+	}
+
+	public void testFromMainLevel() {
 		StringBuffer pageContents = new StringBuffer();
 		pageContents.append("!1 Main title\n");
 		pageContents.append("!2 Firt subtitle\n");
-		pageContents.append("!2 Second subtitle\n");		
-				
+		pageContents.append("!2 Second subtitle\n");
+
 		try {
-			
-			PageTOCWidget toc= new PageTOCWidget(this.widgetRoot(pageContents.toString()), "");
-			
+
+			PageTOCWidget toc = new PageTOCWidget(this.widgetRoot(pageContents
+					.toString()), "");
+
 			String render = unformattedText(toc);
 			assertContains("<div class=\"toc1\">", render);
 			assertContains("<div class=\"contents\">", render);
 			assertContains("<b>Index:</b>", render);
 			assertContains("<div class=\"toc1\">", render);
-			
-			
-			assertContains("<ul><li><a href=\"#Main_title\">Main title</a></li><ul><li><a href=\"#Firt_subtitle\">Firt subtitle</a></li><li><a href=\"#Second_subtitle\">Second subtitle</a></li></ul></ul>", render);
-						
-			assertRef("Main_title","Main title", toc.render());
-			assertRef("Firt_subtitle","Firt subtitle",toc.render());
-			assertRef("Second_subtitle","Second subtitle",toc.render());
-			
+
+			assertContains(
+					"<ul><li><a href=\"#Main_title\">Main title</a></li><ul><li><a href=\"#Firt_subtitle\">Firt subtitle</a></li><li><a href=\"#Second_subtitle\">Second subtitle</a></li></ul></ul>",
+					render);
+
+			assertRef("Main_title", "Main title", toc.render());
+			assertRef("Firt_subtitle", "Firt subtitle", toc.render());
+			assertRef("Second_subtitle", "Second subtitle", toc.render());
+
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
-	
 
-	public void testFromSecondLevel(){		
+	public void testNoLineBreak() {
+		StringBuffer pageContents = new StringBuffer();
+		pageContents.append("!2 Sub Title");
+
+		try {
+
+			PageTOCWidget toc = new PageTOCWidget(this.widgetRoot(pageContents
+					.toString()), "");
+
+			String render = unformattedText(toc);
+			assertContains("<div class=\"toc1\">", render);
+			assertContains("<div class=\"contents\">", render);
+			assertContains("<b>Index:</b>", render);
+			assertContains("<div class=\"toc1\">", render);
+
+			assertContains(
+					"<ul>" + "<li><a href=\"#Sub_Title\">Sub Title</a></li>"
+							+ "</ul>", render);
+
+			assertRef("Sub_Title", "Sub Title", toc.render());
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	public void testFromSecondLevel() {
 		StringBuffer pageContents = new StringBuffer();
 		pageContents.append("!2 First subtitle\n");
-		pageContents.append("!2 Second subtitle\n");		
-				
-		try {			
-			PageTOCWidget toc= new PageTOCWidget(this.widgetRoot(pageContents.toString()), "");
+		pageContents.append("!2 Second subtitle\n");
+
+		try {
+			PageTOCWidget toc = new PageTOCWidget(this.widgetRoot(pageContents
+					.toString()), "");
 			String render = unformattedText(toc);
 			assertContains("<div class=\"contents\">", render);
-			assertContains("<ul><li><a href=\"#First_subtitle\">First subtitle</a></li><li><a href=\"#Second_subtitle\">Second subtitle</a></li></ul>", render);
-			
-			assertRef("First_subtitle","First subtitle", toc.render());
-			assertRef("Second_subtitle","Second subtitle", toc.render());
-			
+			assertContains(
+					"<ul><li><a href=\"#First_subtitle\">First subtitle</a></li><li><a href=\"#Second_subtitle\">Second subtitle</a></li></ul>",
+					render);
+
+			assertRef("First_subtitle", "First subtitle", toc.render());
+			assertRef("Second_subtitle", "Second subtitle", toc.render());
+
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
 
-	public void testMultipleLevels(){		
+	public void testMultipleLevels() {
 		StringBuffer pageContents = new StringBuffer();
 		pageContents.append("Text previous to first secction\n");
 		pageContents.append("!1 Section A\n");
@@ -81,54 +107,49 @@ public class PageTOCWidgetTest extends WidgetTestCase{
 		pageContents.append("!2 Section A.A\n");
 		pageContents.append("Text in section A.A\n");
 		pageContents.append("More text in section A.A\n");
-		pageContents.append("!3 Section A.A.A\n");		
-				
-		try {			
-			PageTOCWidget toc= new PageTOCWidget(this.widgetRoot(pageContents.toString()), "");
+		pageContents.append("!3 Section A.A.A\n");
+
+		try {
+			PageTOCWidget toc = new PageTOCWidget(this.widgetRoot(pageContents
+					.toString()), "");
 			String render = unformattedText(toc);
 			assertContains("<div class=\"contents\">", render);
-			
-			StringBuffer listSTR= new StringBuffer();
+
+			StringBuffer listSTR = new StringBuffer();
 			listSTR.append("<ul>");
 			listSTR.append("<li><a href=\"#Section_A\">Section A</a></li>");
 			listSTR.append("<ul>");
 			listSTR.append("<li><a href=\"#Section_A.A\">Section A.A</a></li>");
 			listSTR.append("<ul>");
-			listSTR.append("<li><a href=\"#Section_A.A.A\">Section A.A.A</a></li>");
+			listSTR
+					.append("<li><a href=\"#Section_A.A.A\">Section A.A.A</a></li>");
 			listSTR.append("</ul>");
 			listSTR.append("</ul>");
 			listSTR.append("</ul>");
-			
-			assertContains(listSTR.toString(), render);		
+
+			assertContains(listSTR.toString(), render);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
-	
 
 	private void assertContains(String expectedSubstring, String actual) {
-		assertTrue("Expected substring \""+expectedSubstring+"\" not found in \""+actual+"\".", actual.contains(expectedSubstring));
+		assertTrue("Expected substring \"" + expectedSubstring
+				+ "\" not found in \"" + actual + "\".", actual
+				.contains(expectedSubstring));
 	}
 
-	private void assertRef(String linkText, String Name,String content) {
-		String expected="<a href=\"#"+linkText+"\">"+Name+"</a>";
-		assertTrue("Expected link \""+linkText+"\" not found in \""+content+"\".", content.contains(expected));
+	private void assertRef(String linkText, String Name, String content) {
+		String expected = "<a href=\"#" + linkText + "\">" + Name + "</a>";
+		assertTrue("Expected link \"" + linkText + "\" not found in \""
+				+ content + "\".", content.contains(expected));
 	}
-	
-	private String unformattedText(PageTOCWidget toc){
-		String render;
-		try {
-			render = toc.render();
-			String contentWithOutFormat=render.replaceAll("\t","");
-			contentWithOutFormat=contentWithOutFormat.replaceAll("\n","");
-			return contentWithOutFormat;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}			
+
+	private String unformattedText(PageTOCWidget toc) throws Exception {
+		String render = toc.render();
+		String contentWithOutFormat = render.replaceAll("\t", "");
+		contentWithOutFormat = contentWithOutFormat.replaceAll("\n", "");
+		return contentWithOutFormat;
 	}
-	
-	
 
 }
